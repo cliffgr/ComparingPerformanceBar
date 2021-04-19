03dcce76f9427454c1891234e1406b309a43ab6d
kostasantoniou <ka@wappier.com>
2021-04-19 22:18:45 +0300

    Added showcase on frgagment

--- app/src/main/java/com/cliff/comparingperformancebr/FirstFragment.kt
+++ app/src/main/java/com/cliff/comparingperformancebr/FirstFragment.kt
@@ -7,15 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.cliff.comparingperformancebar.PercentageProgressBar
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private lateinit var progressBar: PercentageProgressBar
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
@@ -23,6 +26,9 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        progressBar = view.findViewById(R.id.percentageProgressBar)
+        progressBar.setProgress(30.0f)
 
     }
 }
